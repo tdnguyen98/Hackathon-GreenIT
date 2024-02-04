c6d7b1440c4da380da593e20ce1d442bd8882e65
Théo Dinh Nguyen <theo@Ordinateur-portable-de-Theo.local>
2024-02-04 17:54:52 +0100

    version finale script

--- script_test/powermetrics-program.c
+++ script_test/powermetrics-program.c
@@ -47,7 +47,9 @@ int main(int argc, char *argv[]) {
         perror("Error opening file");
         return 1;
     }
-	
+	if (argc != 3 )
+		return 3;
+
     float cpu_wattages[5000];  // Assuming a maximum of 100 entries
     float gpu_wattages[5000];
     float ane_wattages[5000];
@@ -86,7 +88,7 @@ int main(int argc, char *argv[]) {
 
 	float min_gpu = min(gpu_wattages, entry_count);
 	float max_gpu = max(gpu_wattages, entry_count);
-    
+
 	float min_ane = min(ane_wattages, entry_count);
 	float max_ane = max(ane_wattages, entry_count);
 
@@ -97,15 +99,16 @@ int main(int argc, char *argv[]) {
     float deviation_gpu = calculate_deviation(gpu_wattages, average_gpu, entry_count);
     float deviation_ane = calculate_deviation(ane_wattages, average_ane, entry_count);
     float deviation_total = calculate_deviation(total_wattages, average_total, entry_count);
-	
-	
-	float consumption = 0;
-	consumption = (average_total / 12000);
-		
-	
+
+	float consumption = (average_total * atof(argv[2])  / (1000 * 3600 )); // 3600 * 1000 * X seconde de test
+	float cpu_consumption = (average_cpu * atof(argv[2]) / (1000 * 3600 ));
+	float gpu_consumption = (average_gpu  * atof(argv[2]) / (1000 * 3600));
+	float ane_consumption = (average_ane * atof(argv[2]) / (1000 * 3600 ));
+
 	// Print averages
 
 	printf("\n");
+	printf("%s\n", argv[1]);
 	printf("CPU Power min:\t\t%.2f\t\t", min_cpu);
 	if (max_cpu > 10000)
 		printf("CPU Power max: \t\t%.2f\t", max_cpu);
@@ -113,7 +116,7 @@ int main(int argc, char *argv[]) {
 		printf("CPU Power max: \t\t%.2f\t\t", max_cpu);
 	printf("CPU Power avg: \t\t%.2f\t\t", average_cpu);
 	printf("CPU Power deviation: \t\t%.2f\n", deviation_cpu);
-	
+
 	printf("GPU Power min:\t\t%.2f\t\t", min_gpu);
 	if (max_gpu > 10000)
 		printf("GPU Power max: \t\t%.2f\t", max_gpu);
@@ -121,178 +124,24 @@ int main(int argc, char *argv[]) {
 		printf("GPU Power max: \t\t%.2f\t\t", max_gpu);
 	printf("GPU Power avg: \t\t%.2f\t\t", average_gpu);
 	printf("GPU Power deviation: \t\t%.2f\n", deviation_gpu);
-	
+
 	printf("ANE Power min:\t\t%.2f\t\t", min_ane);
 	printf("ANE Power max: \t\t%.2f\t\t", max_ane);
 	printf("ANE Power avg: \t\t%.2f\t\t", average_ane);
 	printf("ANE Power deviation: \t\t%.2f\n", deviation_ane);
-	
+
 	printf("Combined Power min: \t%.2f\t\t", min_tot);
 	if (max_tot > 10000)
 		printf("Combined Power max: \t%.2f\t", max_tot);
 	else
 		printf("Combined Power max: \t%.2f\t\t", max_tot);
-	printf("Combined Power avg:\t%.2f\t\t", average_total);
+	if (average_total > 10000)
+		printf("Combined Power avg:\t%.2f\t", average_total);
+	else
+		printf("Combined Power avg:\t%.2f\t\t", average_total);
 	printf("Combined Power deviation: \t%.2f\n", deviation_total);
 
-	printf("Wh consumed during test: \t%.4f\n", consumption);
-	
-    return 0;
-}
-
-
-/* double	get_value(char *s)
-{
-	char *number;
-	double value;
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			number = &s[i];
-			break;
-		}
-		i++;
-	}
-	value = atof(number);
-	return (value);
-}
-
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    char path[1035];
-	double i = 0;
-	double j = 0;
-	double k = 0;
-	double l = 0;
-	double value_CPU = 0;
-	double total_energy_CPU = 0;
-	double max_value_CPU = 0;
-	double min_value_CPU = 100000;
-	double value_GPU = 0;
-	double total_energy_GPU = 0;
-	double max_value_GPU = 0;
-	double min_value_GPU = 100000;
-	double value_ANE = 0;
-	double total_energy_ANE = 0;
-	double max_value_ANE = 0;
-	double min_value_ANE = 100000;
-	double value_comb = 0;
-	double total_energy_comb = 0;
-	double max_value_comb = 0;
-	double min_value_comb = 100000;
-	double moyenne_CPU = 0;
-	double moyenne_GPU = 0;
-	double moyenne_ANE = 0;
-	double moyenne_comb = 0;
-	double deviation_CPU = 0;
-	double deviation_GPU = 0;
-	double deviation_ANE = 0;
-	double deviation_comb = 0;
-	double consumption = 0;
-
-    // Open the command for reading.
-	if (argc != 2)
-		return (1);
-    fp = popen(argv[1], "r");
-    if (fp == NULL) {
-        printf("Failed to run command\n" );
-        return 1;
-    }
-
-    // Read the output a line at a time - output it.
-    while (fgets(path, sizeof(path)-1, fp) != NULL) {
-        if (strstr(path, "CPU Power") != NULL) {
-            value_CPU = get_value(path);
-			if (value_CPU > max_value_CPU)
-				max_value_CPU = value_CPU;
-			if (value_CPU < min_value_CPU)
-				min_value_CPU = value_CPU;
-			total_energy_CPU += value_CPU;
-			i += 1;
-        }
-		if (strstr(path, "GPU Power") != NULL) {
-            value_GPU = get_value(path);
-			if (value_GPU > max_value_GPU)
-				max_value_GPU = value_GPU;
-			if (value_GPU < min_value_GPU)
-				min_value_GPU = value_GPU;
-			total_energy_GPU += value_GPU;
-			j += 1;
-        }
-		if (strstr(path, "ANE Power") != NULL) {
-            value_ANE = get_value(path);
-			if (value_ANE > max_value_ANE)
-				max_value_ANE = value_ANE;
-			if (value_ANE < min_value_ANE)
-				min_value_ANE = value_ANE;
-			total_energy_ANE += value_ANE;
-			k += 1;
-        }
-		if (strstr(path, "Combined Power") != NULL) {
-             value_comb = get_value(path);
-			if (value_comb > max_value_comb)
-				max_value_comb = value_comb;
-			if (value_comb < min_value_comb)
-				min_value_comb = value_comb;
-			total_energy_comb += value_comb;
-			l += 1;
-        }
-    }
-	// average
-	moyenne_CPU = total_energy_CPU/i;
-	moyenne_GPU = total_energy_GPU/j;
-	moyenne_ANE = total_energy_ANE/k;
-	moyenne_comb = total_energy_comb/l;
-
-	// standard deviation
-	deviation_CPU = sqrt(((total_energy_CPU - moyenne_CPU) * (total_energy_CPU - moyenne_CPU))/(i - 1));
-	deviation_GPU = sqrt(((total_energy_GPU - moyenne_GPU) * (total_energy_GPU - moyenne_GPU))/(j - 1));
-	deviation_ANE = sqrt(((total_energy_ANE - moyenne_ANE) * (total_energy_ANE - moyenne_ANE))/(k - 1));
-	deviation_comb = sqrt(((total_energy_comb - moyenne_comb) * (total_energy_comb - moyenne_comb))/(l - 1));
-
-	// total Wh consumption calculation
-		// X mWatts == (X / 1000) Watts && 1 Watt for 5 minutes = (1 / 12) Watt for 1 hour (Wh)
-	consumption = (moyenne_comb / 12000);
-	
-		
-	printf("\n");
-	printf("CPU Power min:\t\t%.2f\t\t", min_value_CPU);
-	if (max_value_CPU > 10000)
-		printf("CPU Power max: \t\t%.2f\t", max_value_CPU);
-	else
-		printf("CPU Power max: \t\t%.2f\t\t", max_value_CPU);
-	printf("CPU Power avg: \t\t%.2f\t\t", moyenne_CPU);
-	print("CPU Power deviation: \t\t%.2\n", deviation_CPU);
-	
-	printf("GPU Power min:\t\t%.2f\t\t", min_value_GPU);
-	if (max_value_GPU > 10000)
-		printf("GPU Power max: \t\t%.2f\t", max_value_GPU);
-	else
-		printf("GPU Power max: \t\t%.2f\t\t", max_value_GPU);
-	printf("GPU Power avg: \t\t%.2f\n", moyenne_GPU);
-	print("GPU Power deviation: \t\t%.2\n", deviation_GPU);
-	
-	printf("ANE Power min:\t\t%.2f\t\t", min_value_ANE);
-	printf("ANE Power max: \t\t%.2f\t\t", max_value_ANE);
-	printf("ANE Power avg: \t\t%.2f\n", moyenne_ANE);
-	print("ANE Power deviation: \t\t%.2\n", deviation_ANE);
-	
-	printf("Combined Power min: \t%.2f\t\t", min_value_comb);
-	if (max_value_comb > 10000)
-		printf("Combined Power max: \t%.2f\t", max_value_comb);
-	else
-		printf("Combined Power max: \t%.2f\t\t", max_value_comb);
-	printf("Combined Power avg:\t%.2f\n", moyenne_comb);
-	print("Combined Power deviation: \t\t%.2\n", deviation_comb);
-
-		// precision: if 5mW average over 5 minutes, then total consumption is 0.0004 Wh. (Precision: +-1 mW)
-	printf("Wh consumed during test: \t%.4f\n", consumption);
-
-    // close
-    pclose(fp);
+	printf("Wh consumed during test: \t%.4f (CPU: %.4f; GPU: %.4f; ANE: %.4f)\n", consumption, cpu_consumption, gpu_consumption, ane_consumption);
 
     return 0;
-} */
+}
--- script_test/script.sh
+++ script_test/script.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+# This script is used to measure the power consumption of a program
+
+# !!!for now the script only works with a .app program!!!
+
 # Store the first command line argument in a variable
 program_path=$1
 
 # Shut off networking
 networksetup -setairportpower en0 off
 
-# Idle test of power consumption: 1500 samples, every 200ms (5 minutes)
-./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 1500 -i 200 --samplers cpu_power"
+mkdir -p power_consumption
 
+# Idle test of power consumption: §600 samples, every 200ms (2 minute)
+sudo powermetrics -i 200 -n 600 --samplers cpu_power | awk '/CPU Power:/ || /GPU Power:/ || /ANE Power:/ || /Combined Power/ {gsub(/[^0-9]/,""); print}' >> power_consumption/idle_power_consumption.txt
+
+# Benchmark of application power consumption: §600 samples, every 200ms (2 minutes)
+open $program_path & sudo powermetrics -i 200 -n 600 --samplers cpu_power | awk '/CPU Power:/ || /GPU Power:/ || /ANE Power:/ || /Combined Power/ {gsub(/[^0-9]/,""); print}' >> power_consumption/program_power_consumption.txt
+pkill -f $program_path
+
+#input 1: path of the file containing power consumption data, input 2: time in seconds of the benchmark
+./a.out "power_consumption/idle_power_consumption.txt" "120"
 if [ $? -eq 0 ]; then
-# Benchmark of application power consumption: 1500 samples, every 200ms (5 minutes)
-	open $program_path & ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 1500 -i 200 --samplers cpu_power"
- 	sleep 6
-	pkill -f $program_path
+	./a.out "power_consumption/program_power_consumption.txt" "120"
+	if [ $? -eq 0 ]; then
+		echo "\nSuccess"
+	else
+		echo "\nFailed"
+	fi
 else
 	echo "Failed"
 fi
 
+rm -rf power_consumption
+
 # Reactivate networking
 networksetup -setairportpower en0 on
+
