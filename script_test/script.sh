9dead5e6dc0eb0d063667614ac61723e124d7d91
Hullow <129405843+Hullow@users.noreply.github.com>
2024-02-04 13:44:07 +0100

    Update script.sh - sampling

--- script_test/script.sh
+++ script_test/script.sh
@@ -6,12 +6,12 @@ program_path=$1
 # Shut off networking
 networksetup -setairportpower en0 off
 
-# Idle test of power consumption: one sample every 200ms, times 1500 samples: 5 minutes of sampling (300 seconds * 5 times/second)
+# Idle test of power consumption: 1500 samples, every 200ms (5 minutes)
 ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 1500 -i 200 --samplers cpu_power"
 
 if [ $? -eq 0 ]; then
-# Benchmark of application power consumption: 300 samples, every 200ms (1 minute)
-	open $program_path & ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 300 -i 200 --samplers cpu_power"
+# Benchmark of application power consumption: 1500 samples, every 200ms (5 minutes)
+	open $program_path & ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 1500 -i 200 --samplers cpu_power"
  	sleep 6
 	pkill -f $program_path
 else
