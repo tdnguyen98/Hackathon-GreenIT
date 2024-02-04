13f0cc5e57652116c0533d1463955b525008e309
tdnguyen98 <139122698+tdnguyen98@users.noreply.github.com>
2024-02-04 11:47:58 +0100

    Update script.sh killing program

--- script_test/script.sh
+++ script_test/script.sh
@@ -12,6 +12,8 @@ networksetup -setairportpower en0 off
 if [ $? -eq 0 ]; then
 # Benchmark of application power consumption
 	open $program_path & ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 6 -i 1000 --samplers cpu_power"
+ 	sleep 6
+	pkill -f $program_path
 else
 	echo "Failed"
 fi
