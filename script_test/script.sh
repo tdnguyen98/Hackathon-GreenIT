5fbfeba6565935e63bc2a928da0be8256eeb673f
Hullow <129405843+Hullow@users.noreply.github.com>
2024-02-04 11:31:42 +0100

    Update and rename scipts.sh to script.sh - commentaires

--- script_test/script.sh
+++ script_test/script.sh
@@ -3,13 +3,18 @@
 # Store the first command line argument in a variable
 program_path=$1
 
+# Shut off networking
 networksetup -setairportpower en0 off
+
+# Idle test of power consumption
 ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 1 -i 1000 --samplers cpu_power"
 
 if [ $? -eq 0 ]; then
+# Benchmark of application power consumption
 	open $program_path & ./a.out "/usr/bin/sudo /usr/bin/powermetrics -n 6 -i 1000 --samplers cpu_power"
 else
 	echo "Failed"
 fi
 
+# Reactivate networking
 networksetup -setairportpower en0 on
